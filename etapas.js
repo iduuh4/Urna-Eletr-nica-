ea5d8478767693e1fbaca0eeb78f58da2c2ec5bb
EduardoDev <iduuh4@gmail.com>
2024-02-19 18:11:33 -0300

    primeira versão da urna

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Homer',
+                partido: 'Das rosquinhas',
+                fotos:[
+                    {url:'homer.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Bart',
+                partido: 'Dos skatistas',
+                fotos:[
+                    {url:'bart.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '55',
+                nome: 'Bob Esponja',
+                partido: 'Fundo do Mar',
+                vice: 'Patrick',
+                fotos:[
+                    {url:'bob.png', legenda: 'Prefeito'},
+                    {url:'patrick.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '88',
+                nome: 'Pica-Pau',
+                partido: 'Casa da Árvore',
+                vice: 'Leoncio',
+                fotos:[
+                    {url:'picapau.png', legenda: 'Prefeito'},
+                    {url:'leoncio.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
